9312744fafe4a612d7f6673be9078641d8402ff6
Advayth Pashupati <113481915+AamindMandragora@users.noreply.github.com>
2026-04-03 22:21:35 -0500

    send SIGINT to pi

--- shell/run-server.sh
+++ shell/run-server.sh
@@ -8,4 +8,4 @@ rsync -avz --exclude '.git/' --exclude 'objs/' --exclude 'bin/' \
     --exclude '*.exe' --exclude '*.o' --exclude '.vscode/' \
     ./ $PI_HOST:$PI_DIR/
 
-ssh $PI_HOST "cd $PI_DIR && fuser -k 1490/tcp || true && sleep 1 && make clean && make server && ./bin/server_app"
+ssh -t $PI_HOST "cd $PI_DIR && fuser -k 1490/tcp || true && sleep 1 && make clean && make server && ./bin/server_app"
